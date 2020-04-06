a0f725079a586411421a8669a9f2f99e88c135a9
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-04-06 12:48:25 +0530

    template created in KNN.py

--- KNN.py
+++ KNN.py
@@ -5,6 +5,45 @@ Created on Mon Apr  6 11:55:35 2020
 
 @author: po
 """
+import pandas as pd
 
-define test:
-    pass
+
+# creating Ml class
+class Ml:
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+    def __eucli(self):#return the euclidien distance of each elements from the given
+        pass
+    def __select_k(self):# return the k neighbour
+        pass
+    #fn that calculate the class of given data
+    def __ret_class(self):
+        pass
+    #fn run will bw called from outside
+    def run(self,k):
+        self.k = k # amount of neightbour selection
+        
+        
+    
+
+def initial():
+    name = input('Enter the name')
+    height = int(input('enter the height'))
+    weight = int(input('enter the weight'))
+    try:
+        ml = Ml(name,height,weight)
+        ml.run(3)
+    except Exception as e:
+        print(e)
+
+
+initial()
+        
+
+
+'''
+data = pd.read_csv("data.csv")
+print(data)
+'''

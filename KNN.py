6ea2d8bf73ceb4a2610d3bad1d5d8faf0cf5832c
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-04-13 11:41:34 +0530

    creating the dataframe from the csv file

--- KNN.py
+++ KNN.py
@@ -6,6 +6,7 @@ Created on Mon Apr  6 11:55:35 2020
 @author: po
 """
 import pandas as pd
+from math import sqrt
 
 
 # creating Ml class
@@ -14,8 +15,10 @@ class Ml:
         self.name = name
         self.height = height
         self.weight = weight
-    def __eucli(self):#return the euclidien distance of each elements from the given
-        pass
+    def __eucli(self):#return the euclidien distance of each elements from the given as a dataframe
+        self.data['eucli'] =(sqrt((self.height-self.data['height'])**2+(self.weight-self.data['weight'])**2)).astype(float)
+        print(self.data)
+    
     def __select_k(self):# return the k neighbour
         pass
     #fn that calculate the class of given data
@@ -24,6 +27,10 @@ class Ml:
     #fn run will bw called from outside
     def run(self,k):
         self.k = k # amount of neightbour selection
+        self.data = pd.DataFrame(pd.read_csv("data.csv"))
+        print(self.data)
+        self.__eucli()
+        print(self.data)
         
         
     

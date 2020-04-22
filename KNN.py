6c02932070ffaa972859818f8534a86b4932065b
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-04-22 10:37:03 +0530

    error fixed for  type convertion

--- KNN.py
+++ KNN.py
@@ -6,7 +6,8 @@ Created on Mon Apr  6 11:55:35 2020
 @author: po
 """
 import pandas as pd
-from math import sqrt
+import numpy as np
+#from math import sqrt do not use math.sqrt cause error  can not convert the series to class float
 
 
 # creating Ml classf
@@ -15,18 +16,18 @@ class Ml:
         pass
     def add_data(self):
         try:
-            self.name = input('Enter the name')
-            self.height = int(input('enter the height'))
-            self.weight = int(input('enter the weight'))
+            self.name = input('Enter the name :::')
+            self.height = int(input('enter the height :::'))
+            self.weight = int(input('enter the weight :::'))
         except (IOError, TypeError, KeyboardInterrupt) as e:
             print(e)
         except Exception as e:
             print(e)
     def __eucli(self):#return the euclidien distance of each elements from the given as a dataframe
-        self.data['eucli']=self.data['eucli'].astype(float)
-        print(self.data)
-        #self.data['eucli'] =sqrt((self.height-self.data['height'])**2+(self.weight-self.data['weight'])**2).astype(float)
-       # print(self.data)
+        #self.data['eucli']=self.data['eucli'].astype(float)
+        #print(self.data)
+        self.data['eucli'] = np.sqrt((self.height-self.data['height'])**2+(self.weight-self.data['weight'])**2).astype(float)
+        #print(self.data)
     
     def __select_k(self):# return the k neighbour
         pass
@@ -37,7 +38,7 @@ class Ml:
     def run(self,k):
         self.k = k # amount of neightbour selection
         self.data = pd.DataFrame(pd.read_csv("data.csv"))
-        print(self.data)
+       # print(self.data)
         self.__eucli()
         print(self.data)
         

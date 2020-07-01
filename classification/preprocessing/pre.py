8e11714b1b4d38eda783cfaa4f99d1f9d6acaaa2
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-01 12:49:55 +0530

    preprocessing class template updated with features and labels

--- classification/preprocessing/pre.py
+++ classification/preprocessing/pre.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import Imputer
+#from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
 
 
 
@@ -9,8 +10,23 @@ from sklearn.preprocessing import Imputer
 class Preprocess:
     def __init__(self,dataFrame):
         self.data = dataFrame
+        # setting up the independ values into features
+    @property
+    def features(self):
+        return self.x
+    @features.setter
+    def features(self,x):
+        self.x = x
+    @property
+    def labels(self):
+        return self.y
+    @labels.setter
+    def labels(self,y):
+        self.y = y
+    # preprocess the data
     def preprocess(self):
-        self.imputer  = Imputer()
+        self.imputer  = SimpleImputer()
+        
     @property
     def pdf(self):
         return self.data
@@ -24,4 +40,12 @@ class Preprocess:
 # loading the data
 data = pd.read_csv("Data.csv")
 pre = Preprocess(data)
+
 print(pre.pdf)
+
+pre.features = data.iloc[:,:-1].values
+pre.labels = data.iloc[:,3].values
+print(pre.features, pre.labels)
+
+
+

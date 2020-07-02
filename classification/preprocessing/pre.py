395fecd73f762d03b9f4737da3940b58082a6f3c
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-02 10:23:35 +0530

    checking for NaN

--- classification/preprocessing/pre.py
+++ classification/preprocessing/pre.py
@@ -23,10 +23,21 @@ class Preprocess:
     @labels.setter
     def labels(self,y):
         self.y = y
-    # check 
+    # check for nan data column in dataframe
+    def check_nan(self, pri_data=False):
+        self.nan_col=[]
+        for i in self.x.columns:
+            l = list(self.x[i].isnull().values)
+            if any(l):
+                self.nan_col.append(i)
+        return self.nan_col
+    
     # preprocess the data
     def preprocess(self):
         self.imputer  = SimpleImputer()
+        
+        
+        
         
     @property
     def pdf(self):
@@ -42,11 +53,13 @@ class Preprocess:
 data = pd.read_csv("Data.csv")
 pre = Preprocess(data)
 
-print(pre.pdf)
+#print(pre.pdf)
 
 pre.features = data.iloc[:,:-1]
 pre.labels = data.iloc[:,3]
-print(pre.features, pre.labels)
+#print(pre.features, pre.labels)
+
+print(pre.check_nan())
 
 
 

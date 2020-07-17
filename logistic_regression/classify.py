40a05f0ebcfe9d2e0e99c76389b8d79bbc3c3529
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-17 13:15:44 +0530

    preprocessing with feature scaling

---
+++ logistic_regression/classify.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as nm
+import matplotlib.pyplot as plt
+# import the module spliting the dataset
+from sklearn.model_selection import train_test_split
+
+
+data = pd.read_csv("Social_Network_Ads.csv")
+print(data)
+'''
+the dataset contains the data collected of people who can buy a suv
+the prediction has take place by using two independent properties which is age and
+salary 
+finding out the correlation between the age and salary and corresponding output
+
+
+
+'''
+
+# setting up the the feature as well as the class matrices
+x = data.iloc[:,[2,3]].values
+y = data.iloc[:,4].values
+
+#Spliting the dataset for testing and training . 100 elements for testing 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .25,random_state=0)
+
+# scaling the feature matrix 
+from sklearn.preprocessing import StandardScaler
+
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.fit_transform(x_test)
+
+
+
+
+
+
+
+
+
+
+

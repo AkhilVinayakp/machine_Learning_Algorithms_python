3901ec5d0f2d5aae484bbe2c4fae368c6d710c74
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-02-26 09:07:24 +0530

    creating dendrograms

--- ml_camp/hier_clust.py
+++ ml_camp/hier_clust.py
@@ -1,8 +1,33 @@
+"""
+Hierarchical clustering 
+with alggomerative method
+visualizing the dentogram
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as nm
 import logging
+from pandas.io.parsers import read_csv
+import scipy
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-# reading data
-df = pd.read_csv('eurovision.csv')
-print(df.head(3))
+
+class Hier:
+    def __init__(self) -> None:
+        self.data = pd.read_csv('eurovision.csv')
+    def __str__(self):
+        return str(self.data.info())
+    def explore(self):
+        print(self.data.head(3))
+        print("Tail of data")
+        print(self.data.tail(3))
+    def pre(self):
+        # checking for clumns contains any NaN
+        if any(self.data.isnull()):
+            self.data.dropna()
+            print('Droped')
+
+
+    # def create_d(self):
+
+    
--- ml_camp/hier_clust_test.py
+++ ml_camp/hier_clust_test.py
@@ -1,4 +1,17 @@
 # basic tesing file for Hierarchical Clustering 
+
 from hier_clust import *
 import unittest
-class TestMe(unittest.D)
+class TestMe(unittest.TestCase):
+    def setUp(self) -> None:
+        self.t = Hier()
+        print(self.t)
+    @unittest.skip('skipping str')
+    def test_1(self):
+        print(str(self.t))
+    def test_3(self):
+        self.t.pre()
+        print(self.t.shape)
+
+if __name__ == "__main__":
+    unittest.main()
